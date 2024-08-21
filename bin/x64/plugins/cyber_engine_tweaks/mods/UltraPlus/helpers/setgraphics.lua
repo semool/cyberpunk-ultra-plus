8d5bac73c7a9ad21c33c9fb30539a0809a9b74b5
semool <semool@protonmail.com>
2024-08-21 13:33:43 +0200

    myownsettings

--- bin/x64/plugins/cyber_engine_tweaks/mods/UltraPlus/helpers/setgraphics.lua
+++ bin/x64/plugins/cyber_engine_tweaks/mods/UltraPlus/helpers/setgraphics.lua
@@ -10,21 +10,25 @@ function Config.SetGraphics(graphics)
 
 	if graphics == Var.graphics.HIGH then
 		LoadIni('graphics_high')
+		LoadIni('myownsettings')
 		return
 	end
 
 	if graphics == Var.graphics.MEDIUM then
 		LoadIni('graphics_medium')
+		LoadIni('myownsettings')
 		return
 	end
 
 	if graphics == Var.graphics.LOW then
 		LoadIni('graphics_low')
+		LoadIni('myownsettings')
 		return
 	end
 
 	if graphics == Var.graphics.POTATO then
 		LoadIni('graphics_potato')
+		LoadIni('myownsettings')
 		return
 	end
 end
